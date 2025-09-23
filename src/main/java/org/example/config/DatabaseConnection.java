e376e95f70701216061940aa1c18362a57f479dd
zakariae elmoufid <moufidzakarie3@gmail.com>
2025-09-23 12:54:54 +0100

    define structur file and connect with database

---
+++ src/main/java/org/example/config/DatabaseConnection.java
@@ -0,0 +1,25 @@
+package org.example.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/Digital-Bank"; // fix DB name
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "123123";
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Connected to PostgreSQL database!");
+            } catch (SQLException e) {
+                System.err.println("Connection failed: " + e.getMessage());
+            }
+        }
+        return connection;
+    }
+}
