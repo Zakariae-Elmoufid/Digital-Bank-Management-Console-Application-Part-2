0bbfda7c0e0fdcb6e3ebf18b64bcdb03b111d38e
zakariae elmoufid <moufidzakarie3@gmail.com>
2025-10-06 20:33:42 +0100

    last commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
 import org.example.config.DatabaseConnection;
+import org.example.controllers.AuthController;
+import org.example.repositories.UserRepository;
+import org.example.services.AuthService;
+import org.example.views.MenuAuth;
 
 import java.sql.Connection;
 import java.sql.SQLOutput;
@@ -9,9 +13,11 @@ import java.sql.SQLOutput;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-          Connection conn = DatabaseConnection.getConnection();
-          if(conn!=null){
-              System.out.println("Connection successful!");
-          }
+//        UserRepository userRepository = new UserRepository();
+//        AuthService authService = new AuthService(userRepository);
+//        AuthController authController = new AuthController(authService);
+
+        MenuAuth  menu = new MenuAuth();
+        menu.showMenu();
     }
 }
